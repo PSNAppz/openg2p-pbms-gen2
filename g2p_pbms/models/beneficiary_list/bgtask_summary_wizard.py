e7fc2642a3f66c10f7c4ded6553d8f443975814d
PSNAppZ <thepsnarayanan@gmail.com>
2026-05-15 10:40:31 +0530

    Add logger

--- g2p_pbms/models/beneficiary_list/bgtask_summary_wizard.py
+++ g2p_pbms/models/beneficiary_list/bgtask_summary_wizard.py
@@ -504,6 +504,7 @@ class G2PBGTaskSummaryWizard(models.TransientModel):
             response = requests.post(endpoint, json=payload, headers=headers, timeout=10)
             response.raise_for_status()
             response_json = response.json()
+            _logger.info("get_beneficiaries response: %s", response_json)
         except Exception as e:
             _logger.error("API call failed: %s", e)
             return {
